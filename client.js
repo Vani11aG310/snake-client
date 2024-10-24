9c566bae7252e7899755c4cdeb216b8bfb1df34c
Angelo <angvil02@gmail.com>
2024-10-23 19:34:58 -0700

    experimented with move commands

--- client.js
+++ client.js
@@ -1,19 +1,28 @@
 const net = require("net");
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: 'localhost', // IP address here,
     port: 50541 // PORT number here,
   });
   conn.on('connect', () => {
-    console.log('Successfully connected to game server')
-    conn.write("Name: AV")
-  })
+    console.log('Successfully connected to game server');
+    conn.write("Name: AV");
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    //   setTimeout(() => {
+    //     conn.write("Move: right");
+    //   }, 50)
+    // }, 50);
+    // setInterval(() => {
+    //   conn.write("Move: up")
+    // }, 50)
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on('data', (data) => {
     console.log(data);
-  })
+  });
   return conn;
 };
 
